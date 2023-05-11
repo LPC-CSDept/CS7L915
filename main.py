e1a81b7ca27f221727cbdeeaa0f0d6e1e8b3d8f6
Kyu Lee <kwooler@gmail.com>
2023-05-11 14:33:35 -0700

    CS7L915

--- main.py
+++ main.py
@@ -1,18 +1,9 @@
 
-gt = (lambda x: x[0] > x[1])
-first = (lambda x: x[0])
-
-
 def main():
     mylist = [5, 10, 15, 25, 20, 55, 40]
     #########################################
     # Code your program here
     #########################################
-
-    zipped = zip(mylist[1:], mylist)
-    result = list(filter(gt, zipped))
-    result = list(map(first, result))
-    print(result)
 
     #########################################
     # Do not delete the reutrn statement
